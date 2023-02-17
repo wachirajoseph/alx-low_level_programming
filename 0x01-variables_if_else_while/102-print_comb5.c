1062a017acb2cf0ba046547c070814cc9e1657ce
wachirajoseph <wachirajoseph168@gmail.com>
2023-02-17 07:47:41 -0800

    task 12 resolved

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,38 +8,29 @@
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+	int n;
+	int i;
+	int h;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+	for (h = 0 ; h != 8 ; h++)
 	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
+		for (n = h + 1 ; n != 9 ; n++)
 		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
+			i = n + 1;
+			do {
+				putchar('0' + h);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (h != 7)
 				{
 					putchar(',');
 					putchar(32);
 				}
-				bas++;
-			}
+				i++;
+			} while (i != 10);
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
-
 
