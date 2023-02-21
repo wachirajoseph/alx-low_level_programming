d49f24339d877548bf4ce3f875a3cde37661a06f
wachirajoseph <wachirajoseph168@gmail.com>
2023-02-21 06:23:00 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Function prototype
+ * Description: Prints the letters of the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+
+	_putchar('\n');
+}
+Footer
+
