91859aca5baa142c0f35e7a38dc4b8e03896bed4
wachirajoseph <wachirajoseph168@gmail.com>
2023-02-21 06:51:20 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Function prototype
+ * Description: Adds two intergers together
+ * @x: The first number to add
+ * @y: The second number to add
+ * Return: The sum of x and y
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
+
