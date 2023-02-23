332ed358748059ad514d52c1affd68639106ab8d
wachirajoseph <wachirajoseph168@gmail.com>
2023-02-23 12:46:53 -0800

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
 
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
 
